e6951a66f7355d909898c8f43e39281fcf6a5a45
Brandon Clapp <brandonclapp@outlook.com>
2023-07-13 16:17:00 -0500

    Add fretboard package

---
+++ packages/fretboard/src/lib/fretboard.spec.ts
@@ -0,0 +1,7 @@
+import { fretboard } from './fretboard';
+
+describe('fretboard', () => {
+  it('should work', () => {
+    expect(fretboard()).toEqual('fretboard');
+  });
+});
---
+++ packages/fretboard/src/lib/fretboard.ts
@@ -0,0 +1,3 @@
+export function fretboard(): string {
+  return 'fretboard';
+}
