719110a8ce1735de0e0115087bfacf851e4100c9
Brandon Clapp <brandonclapp@outlook.com>
2023-07-13 16:42:48 -0500

    Remove fretboard

--- packages/fretboard/src/lib/fretboard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { fretboard } from './fretboard';
-
-describe('fretboard', () => {
-  it('should work', () => {
-    expect(fretboard()).toEqual('fretboard');
-  });
-});
--- packages/fretboard/src/lib/fretboard.ts
+++
@@ -1,3 +0,0 @@
-export function fretboard(): string {
-  return 'fretboard';
-}
